42bed2fb06a0ddb58665cc2df2dc3d8b14880c83
nagannaCtI064 <119935869+nagannaCtI064@users.noreply.github.com>
2023-01-10 10:40:00 +0530

    Add files via upload

---
+++ prog2.js
@@ -0,0 +1,18 @@
+a=parseInt(prompt("enter a number value of a:"))
+b=parseInt(prompt("enter a number value of b:"))
+function add(){
+c=a+b
+document.getElementById("demo1").innerHTML=c
+}
+function sub(){
+c=a-b
+document.getElementById("demo2").innerHTML=c
+}
+function mul(){
+c=a*b
+document.getElementById("demo3").innerHTML=c
+}
+function div(){
+c=a/b
+document.getElementById("demo4").innerHTML=c
+}
